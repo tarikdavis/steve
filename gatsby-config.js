ae9a775d93eabbbed6c84897627c1be7af92dfa8
Tarik <tarikdavis@hotmail.co.uk>
2020-01-15 20:54:02 +0000

    second commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-tailwindcss`,
+      options: {
+        postCssPlugins: [require("autoprefixer")],
+      },
+    },
+  ],
+}
